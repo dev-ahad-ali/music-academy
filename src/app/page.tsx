ddba7f5ca5a8a9662629bc3ae572f675e80bb95b
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-25 02:39:34 +0600

    Update: navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
+    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
       <h2 className='text-3xl text-center py-4'>Main Page</h2>
-    </div>
+    </main>
   );
 }
