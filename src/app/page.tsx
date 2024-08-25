22445bdcabb4aafdb79b5aa3fbc5c8a14771144f
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-25 19:02:07 +0600

    Update: Testimonial section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FeaturedCourses from '@/components/FeaturedCourses';
 import HeroSection from '@/components/HeroSection';
+import TestimonialCard from '@/components/TestimonialCard';
+import WhyChooseUs from '@/components/WhyChooseUs';
 
 export default function Home() {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCard />
     </main>
   );
 }
